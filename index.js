431d2a048dc86699f85dde075c7799ca069b3ba8
riskayulia <riskayulianaa0102@gmail.com>
2022-11-02 11:39:43 +0700

    riska tugas percabangan

---
+++ index.js
@@ -0,0 +1,92 @@
+//console.log("hello world")
+
+//var a = 12;
+
+//console.log(a);
+
+//operator pada aritmatika (* + - /)
+
+
+// var r = 11;
+// var r1 = 12;
+
+// var hasil = ( bil1 + bil2) 
+// hasil1 = (bil1/bil2);
+// hasil2 = (bil1-bil2);
+// hasil3 = (bil1*bil2);
+
+// console.log(hasil);
+// console.log(hasil1);
+// console.log(hasil2);
+// console.log(hasil3);
+
+// var hasil = 3.14*(r * r1);
+// console.log(hasil)
+
+// operator logika
+//|| &&
+
+
+// benar || benar =true
+// benar || salah =true
+// salah || benar =true
+// salah || salah =false
+
+// benar && benar =true
+// benar && salah =false
+// salah && benar =false
+// salah && salah =false
+
+// let nilai = 80;
+// console.log(a)
+
+//let nilai =90
+//if(nilai > 50){
+    //console.log("selamat kamu lulus");
+//}else{
+//console.log("kamu harus belajar lagi");
+// }  
+//let nilai = 102;
+
+
+//if (nilai >=90 && nilai <= 100){
+    //console.log(nilai +"nilai sangat memuaskan");
+//} else if(nilai >=80 && nilai <= 89){
+    //console.log(nilai + "nilai memuaskan");
+//} else if(nilai >=70 && nilai <=79 ){
+    //console.log(nilai + "nilai cukup");
+//}else{
+    //console.log(nilai + "nilai kurang");
+//}
+
+
+
+// const email = "riskayulianaa0102@gmail.com";
+// const password = "123456789";
+
+// if (email ==="riskayulianaa0102@gmail.com"){
+//     if (password ==="123456789"){
+//         console.log("kamu berhasil login");
+// } else {
+//     console.log("password salah");
+// }
+// } else {
+//     console.log("email belum terdaftar");
+// }
+
+
+// switch case
+
+const warna = 'hitam';
+
+switch (warna) {
+    case "hitam":
+        console.log("warnaa hitam");
+        break;
+    case "putih":
+        console.log("warna putih");
+    break;
+    default:
+        console.log("tidaka ada warna pilihan");
+        break;        
+}
